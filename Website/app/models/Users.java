02301fa8071529029a4bf3546082bc3223dde0a4
Max Pearson <m.pearson-11@student.lboro.ac.uk>
2015-09-28 14:56:30 +0100

    Added Users Model

---
+++ Website/app/models/Users.java
@@ -0,0 +1,18 @@
+package models;
+
+import play.*;
+import play.mvc.*;
+import play.db.*;
+
+import play.db.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+import java.sql.*;
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+public class Users extends Model {
+
+}
