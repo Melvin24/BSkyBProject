c053847f50db5427c48c88ac5195f5b1c38c3ac7
Schofield, Nicola (Associate Software Developer) <nicola.schofield@sky.uk>
2015-09-29 10:47:54 +0100

    More work on customer UI

--- Website/app/controllers/Application.java
+++ Website/app/controllers/Application.java
@@ -12,11 +12,16 @@ import views.html.*;
 import models.*;
 
 public class Application extends Controller {
+  /*
+    public Result index() {
+         return ok(products.render("list of all the products"));
+       // List<Customer> c = Customer.find.all();
+        //List<Users> u = Users.find.all();
+        //return ok(index.render(c,u));
+    }*/
 
     public Result index() {
-        List<Customer> c = Customer.find.all();
-        List<Users> u = Users.find.all();
-        return ok(index.render(c,u));
+         return ok(home.render("Advertisments and shop"));
     }
 
     public Result products() {
@@ -25,6 +30,10 @@ public class Application extends Controller {
 
     public Result item(String itemName) {
       return ok(item.render(itemName));
+    }
+
+    public Result faq() {
+      return ok(faq.render("FAQ page"));
     }
 
 
