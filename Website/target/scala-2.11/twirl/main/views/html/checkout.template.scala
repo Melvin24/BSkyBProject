2df89af7a677c4d2f0e636d69b8becd4a835f38f
Ronan Spoor <ronan.spoor@sky.uk>
2015-09-30 15:53:57 +0100

    First draft.
    Working.

--- Website/target/scala-2.11/twirl/main/views/html/checkout.template.scala
+++ Website/target/scala-2.11/twirl/main/views/html/checkout.template.scala
@@ -38,33 +38,44 @@ Seq[Any](format.raw/*1.28*/("""
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
   <div class="container">
-        """),_display_(/*11.10*/helper/*11.16*/.form(action = routes.Application.save() )/*11.58*/{_display_(Seq[Any](format.raw/*11.59*/("""
-            """),format.raw/*12.13*/("""<div class="row">
-            """),_display_(/*13.14*/helper/*13.20*/.inputText(checkout("forename"))),format.raw/*13.52*/("""
-            """),format.raw/*14.13*/("""</div>
+      You are signed in, please checkout using the form below...
+  </div>
+</div>
+
+<div class="container">
+"""),_display_(/*16.2*/helper/*16.8*/.form(action = routes.Application.save() )/*16.50*/{_display_(Seq[Any](format.raw/*16.51*/("""
+            """),format.raw/*17.13*/("""<div class="row">
+            """),_display_(/*18.14*/helper/*18.20*/.inputText(checkout("forename"))),format.raw/*18.52*/("""
+            """),format.raw/*19.13*/("""</div>
             <div class="row">
                 <div class="col-xs-12">
-                """),_display_(/*17.18*/helper/*17.24*/.inputText(checkout("surname"))),format.raw/*17.55*/("""
-                """),format.raw/*18.17*/("""</div>
+                """),_display_(/*22.18*/helper/*22.24*/.inputText(checkout("surname"))),format.raw/*22.55*/("""
+                """),format.raw/*23.17*/("""</div>
             </div>
             <div class="row">
                 <div class="col-xs-12">
-                """),_display_(/*22.18*/helper/*22.24*/.inputText(checkout("sky_id"))),format.raw/*22.54*/("""
-                """),format.raw/*23.17*/("""</div>
+                """),_display_(/*27.18*/helper/*27.24*/.inputText(checkout("sky_customer_id"))),format.raw/*27.63*/("""
+                """),format.raw/*28.17*/("""</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                """),_display_(/*32.18*/helper/*32.24*/.inputText(checkout("address_id"))),format.raw/*32.58*/("""
+                """),format.raw/*33.17*/("""</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                """),_display_(/*37.18*/helper/*37.24*/.inputText(checkout("customer_id"))),format.raw/*37.59*/("""
+                """),format.raw/*38.17*/("""</div>
             </div>
             <div class="row">
                 <div class="col-xs-12">
                  <input type="submit" value="SHIP" class="btn primary">
                 </div>
             </div>
-        """)))}),format.raw/*30.10*/("""
-        """),format.raw/*31.9*/("""</div>
-    </div>
-<div class="container">
+        """)))}),format.raw/*45.10*/("""
+"""),format.raw/*46.1*/("""</div>
 
-</div>
-
-""")))}),format.raw/*37.2*/("""
+""")))}),format.raw/*48.2*/("""
 """))
       }
     }
@@ -85,11 +96,11 @@ Seq[Any](format.raw/*1.28*/("""
 object checkout extends checkout_Scope0.checkout
               /*
                   -- GENERATED --
-                  DATE: Wed Sep 30 15:17:37 BST 2015
+                  DATE: Wed Sep 30 15:47:52 BST 2015
                   SOURCE: /Users/rsp04/Websites/Wednesday/BSkyBProject/Website/app/views/checkout.scala.html
-                  HASH: 47a21c7b45da8c1840644b4136bac05a28efe3dc
-                  MATRIX: 759->1|895->27|923->46|950->48|994->84|1033->86|1061->88|1239->239|1254->245|1305->287|1344->288|1385->301|1443->332|1458->338|1511->370|1552->383|1673->477|1688->483|1740->514|1785->531|1925->644|1940->650|1991->680|2036->697|2286->916|2322->925|2404->977
-                  LINES: 27->1|32->1|34->4|35->5|35->5|35->5|37->7|41->11|41->11|41->11|41->11|42->12|43->13|43->13|43->13|44->14|47->17|47->17|47->17|48->18|52->22|52->22|52->22|53->23|60->30|61->31|67->37
+                  HASH: 291b89eeeddda4dcbe598e92d7ab6aaa8b0a694f
+                  MATRIX: 759->1|895->27|923->46|950->48|994->84|1033->86|1061->88|1336->337|1350->343|1401->385|1440->386|1481->399|1539->430|1554->436|1607->468|1648->481|1769->575|1784->581|1836->612|1881->629|2021->742|2036->748|2096->787|2141->804|2281->917|2296->923|2351->957|2396->974|2536->1087|2551->1093|2607->1128|2652->1145|2902->1364|2930->1365|2969->1374
+                  LINES: 27->1|32->1|34->4|35->5|35->5|35->5|37->7|46->16|46->16|46->16|46->16|47->17|48->18|48->18|48->18|49->19|52->22|52->22|52->22|53->23|57->27|57->27|57->27|58->28|62->32|62->32|62->32|63->33|67->37|67->37|67->37|68->38|75->45|76->46|78->48
                   -- GENERATED --
               */
           
